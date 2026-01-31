1e42d01edc17242fad89788e7f37c07f606e26a1
sanjana7821 <sanjanakumari7821@gmail.com>
2026-01-31 13:50:27 +0530

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.php",
+    "./scripts.js",
+    "./src/**/*.php",
+    "./*.php",
+    "*.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
